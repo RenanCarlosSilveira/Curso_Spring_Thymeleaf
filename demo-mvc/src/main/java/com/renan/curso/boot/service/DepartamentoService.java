1147e19b48aeb7c73a45871befaea496166b2179
RenanCarlosSilveira <71138883+RenanCarlosSilveira@users.noreply.github.com>
2022-03-23 21:15:20 -0300

    Commit ref 23-03-22
    Referente a 23/03/22

--- demo-mvc/src/main/java/com/renan/curso/boot/service/DepartamentoService.java
+++ demo-mvc/src/main/java/com/renan/curso/boot/service/DepartamentoService.java
@@ -1,5 +1,19 @@
 package com.renan.curso.boot.service;
 
-public interface DepartamentoService {
+import java.util.List;
 
+import com.renan.curso.boot.domain.Departamento;
+
+public interface DepartamentoService {
+	void salvar(Departamento departamento);
+	
+	void editar(Departamento departamento);
+	
+	void excluir(Long id);
+	
+	Departamento buscarPorId(Long id);
+	
+	List<Departamento> buscarTodos(); 
+	
+	boolean departamentoTemCargos(Long id);
 }
