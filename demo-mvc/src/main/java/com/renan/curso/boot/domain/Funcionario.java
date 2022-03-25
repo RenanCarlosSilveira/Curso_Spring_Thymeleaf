d2ebf89a3f67f68d04f7450894ce0ed4d740ea6d
RenanCarlosSilveira <71138883+RenanCarlosSilveira@users.noreply.github.com>
2022-03-25 16:58:00 -0300

    Aula 50 Concluída

--- demo-mvc/src/main/java/com/renan/curso/boot/domain/Funcionario.java
+++ demo-mvc/src/main/java/com/renan/curso/boot/domain/Funcionario.java
@@ -5,6 +5,11 @@ import java.time.LocalDate;
 
 import javax.persistence.*;
 
+import org.springframework.format.annotation.*;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import groovyjarjarpicocli.CommandLine.Help.Ansi.Style;
+
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "FUNCIONARIOS")
@@ -13,12 +18,15 @@ public class Funcionario extends AbstractEntity<Long> {
 	@Column(nullable = false, unique = true)
 	private String nome;
 	
+	@NumberFormat( pattern = "#,##0.00") //style = Style.CURRENCY,
 	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
 	private BigDecimal salario;
 	
+	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name= "data_entrada", nullable = false, columnDefinition = "DATE")
 	private LocalDate dataEntrada;
 	
+	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name = "data_saida", columnDefinition = "DATE")
 	private LocalDate dataSaida;
 	
